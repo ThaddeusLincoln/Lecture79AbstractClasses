ba0da3f7417de8e226e36fca65ed4713843bc698
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-15 23:29:52 +0200

    Working like a charm the SearchTree, which shows everything ordered when traverse() is called.

--- Lect79AbstractClasses/src/com/turing/jdev/abstractclasses/test/Main.java
+++ Lect79AbstractClasses/src/com/turing/jdev/abstractclasses/test/Main.java
@@ -3,12 +3,14 @@ package com.turing.jdev.abstractclasses.test;
 import com.turing.jdev.abstractclasses.Dog;
 import com.turing.jdev.abstractclasses.Parrot;
 import com.turing.jdev.abstractclasses.Penguin;
+import com.turing.jdev.abstractclasses.searchtree.Node;
+import com.turing.jdev.abstractclasses.searchtree.SearchTree;
 
 public class Main {
 	
 	public static void main(String[] args){
 	
-		Dog dog = new Dog("Back");
+		/*Dog dog = new Dog("Back");
 		dog.eat();
 		dog.breathe();
 		
@@ -18,7 +20,20 @@ public class Main {
 		parrot.fly();
 		
 		Penguin skipper = new Penguin("Skipper");
-		skipper.fly();
+		skipper.fly();*/
+		
+		// TEST SEARCH TREE
+		SearchTree tree = new SearchTree(null);
+		
+		// String stringData = "5 7 3 9 8 2 1 0 4 6";
+		String stringData = "Darwing Brisbane Perth Merbourne Camberra Adelaide Sydney Camberra";
+		
+		String[] data = stringData.split(" ");
+		for(String s : data){
+			tree.addItem(new Node(s));
+		}
+		
+		tree.traverse(tree.getRoot()); 		// prints out everything ordered, so cool!
 		
 	}
 }
